41bb7d49630530794e0999fd4c8fab7268422e20
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-12-02 23:06:08 +0000

    cargo fmt

--- src/bin/day1p2.rs
+++ src/bin/day1p2.rs
@@ -1,8 +1,9 @@
 use std::io::{self, BufRead};
 
 fn main() {
-
-    let nums: Vec<u64> = io::stdin().lock().lines()
+    let nums: Vec<u64> = io::stdin()
+        .lock()
+        .lines()
         .map(|l| l.unwrap())
         .map(|l| l.parse().expect("int parse error"))
         .collect();
@@ -13,18 +14,20 @@ fn main() {
         count: usize,
     }
 
-    let State {count, ..} = nums.windows(3)
-        .fold(State::default(), |State {prev, mut count}, x| {
-            if let Some(prev) = prev {
-                if x.iter().sum::<u64>() > prev.iter().sum::<u64>() {
-                    count += 1;
+    let State { count, .. } =
+        nums.windows(3)
+            .fold(State::default(), |State { prev, mut count }, x| {
+                if let Some(prev) = prev {
+                    if x.iter().sum::<u64>() > prev.iter().sum::<u64>() {
+                        count += 1;
+                    }
                 }
-            }
 
-            State { prev: Some(x.to_owned()) , count }
-        });
+                State {
+                    prev: Some(x.to_owned()),
+                    count,
+                }
+            });
 
     dbg!(count);
 }
-
-
